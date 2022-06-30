78d6253d062403c233c39929291644a9415117ac
HarryPotter04 <omujifrank4@gmail.com>
2022-06-30 13:27:23 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - To print out "with proper grammar, but the outcome is a piece of art,"
+ * main - To print "with proper grammar, but the outcome is a piece of art,"
  *
  * Return: Always 0 (Success)
  */

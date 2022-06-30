a8c416f22df650e66b78a9f64cf07cba98a06f19
HarryPotter04 <omujifrank4@gmail.com>
2022-06-30 13:06:52 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,11 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Programming is like building a multilingual puzzle");
+	printf("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 

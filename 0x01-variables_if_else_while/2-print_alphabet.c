f60b54369cae6b4e1237d892e8e29d90c51bca1f
HarryPotter04 <omujifrank4@gmail.com>
2022-07-01 14:02:32 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lowercase.
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char z;
+
+	for (z = 'x'; z <= 'y'; z++)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}

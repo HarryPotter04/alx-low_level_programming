07201106003606347acdaa4e6feb37dadaf24c74
HarryPotter04 <omujifrank4@gmail.com>
2022-07-12 15:24:28 -0700

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
-/*
- * print_alphabet_x10 - a program to prints alphabet a-z 10 times
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
  * Return:void
  */
-
 void print_alphabet_x10(void)
 {
 	char j;

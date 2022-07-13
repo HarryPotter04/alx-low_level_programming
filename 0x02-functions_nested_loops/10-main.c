5795afedcdb8ffeb219b6deda936cfd12d17803a
HarryPotter04 <omujifrank4@gmail.com>
2022-07-13 05:46:39 -0700

    9. Learn your times table && 10. a + b

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int j;
+
+	j = add(89, 9);
+	printf("%d\n", j);
+	return (0);
+}
